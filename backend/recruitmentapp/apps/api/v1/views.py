0ce64108533370497d98692a424469eff96ac8e9
Kim Askebris <askebriskim@gmail.com>
2018-02-07 16:53:30 +0100

    create applicant viewset

--- backend/recruitmentapp/apps/api/v1/views.py
+++ backend/recruitmentapp/apps/api/v1/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
 
-# Create your views here.
+from rest_framework.response import Response
+
+
+from recruitmentapp.apps.api.v1.serializers import ApplicantSerializer
+from recruitmentapp.apps.core.models import Applicant
+
+
+class ApplicantViewSet(viewsets.GenericViewSet):
+
+    queryset = Applicant.objects.all()
+    serializer = ApplicantSerializer
+
+    def retrieve(self, request, pk=None):
+        applicant = get_object_or_404(self.get_queryset(), pk=pk)
+        serializer = self.get_serializer()
+        serializer(applicant)
+        return Response(serializer.data)
+
+    def list(self):
+        applicants = self.get_queryset()
+        serializer = self.get_serializer()
+        serializer(applicants, many=True)
+        return Response(serializer.data)
+
+
