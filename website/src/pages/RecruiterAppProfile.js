2f2cec5fcc22e41292ea0aa59a962ad4fe5b1571
Kim Askebris <askebriskim@gmail.com>
2018-02-23 13:09:18 +0100

    add internationalization

--- website/src/pages/RecruiterAppProfile.js
+++ website/src/pages/RecruiterAppProfile.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 // import {DefaultRoute, Switch, Route, Link} from 'react-router-dom';
 import Navigation from '../components/Navigation';
 import PageHeader from '../components/PageHeader';
+import { FormattedMessage } from 'react-intl';
 
 class RecruiterAppProfile extends Component {
 	render() {
 		return (
 			<div>
-				<PageHeader>Profile</PageHeader>
-				Recuiter profile page.
+				<PageHeader>
+					<FormattedMessage id="recruiterProfileHeader" defaultMessage="Profile" />
+				</PageHeader>
+				<FormattedMessage id="recruiterProfile" defaultMessage="Recruiter profile page." />
 			</div>
 		);
 	}
